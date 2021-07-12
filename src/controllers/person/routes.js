5534d987b9f4490ec3a9fb7d1dc495a01a77101e
sistiri <sistiri@yahoo.com>
2021-07-12 17:32:04 +0200

    03-Routing - CRUD Methods

--- src/controllers/person/routes.js
+++ src/controllers/person/routes.js
@@ -3,8 +3,54 @@ const data = require('./data');
 
 const controller = express.Router();
 
+// Get data.
+
 controller.get('/', (req, res) => {
   res.json(data);
+});
+
+// Get one person.
+
+controller.get('/:id', (req, res) => {
+  const person = data.find(p => p.id === Number(req.params.id));
+  res.json(person);
+});
+
+// Create a new Person.
+
+controller.post('/', (req, res) => {
+  const newPerson = req.body;
+  newPerson.id = data[data.length - 1].id + 1;
+  data.push(newPerson);
+
+  res.status(201);
+  res.json(newPerson);
+});
+
+// Update a person.
+
+controller.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const index = data.findIndex(p => p.id === Number(id));
+  const { first_name, last_name, email } = req.body;
+  
+  data[index] = {
+    id,
+    first_name,
+    last_name,
+    email
+  };
+
+  
+  res.json(data[index]);
+});
+
+// Delete a person.
+
+controller.delete('/:id', (req, res) => {
+  const index = data.findIndex(p => p.id === Number(req.params.id));
+  data.splice(index, 1);
+  res.json({});
 });
 
 module.exports = controller;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
+app.use(bodyParser.json());
 app.use('/person', require('./controllers/person/routes'));
 
 app.listen(port, () => {
