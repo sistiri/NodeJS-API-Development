2b8356864b8747ec793f7cc042657e4d22667a25
sistiri <sistiri@yahoo.com>
2021-07-13 13:06:29 +0200

    05-Logging - Winston, Morgan modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const logger = require('./config/logger')
+
 const port = 3000;
+
+app.use(morgan('combined', { stream: logger.stream }));
 
 app.use(bodyParser.json());
 app.use('/person', require('./controllers/person/routes'));
