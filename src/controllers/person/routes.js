30abc9e90b943be6e1618737e78ef61b41b421f4
sistiri <sistiri@yahoo.com>
2021-07-13 12:22:16 +0200

    04-Error-Handling

--- src/controllers/person/routes.js
+++ src/controllers/person/routes.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const data = require('./data');
+const createError = require('http-errors');
 
 const controller = express.Router();
 
 // Get data.
 
 controller.get('/', (req, res) => {
-  res.json(data);
+   res.json(data);
 });
 
 // Get one person.
 
-controller.get('/:id', (req, res) => {
+controller.get('/:id', (req, res, next) => {
   const person = data.find(p => p.id === Number(req.params.id));
+  if (!person) {
+    return next(new createError.NotFound("Person is not found"));
+  }
   res.json(person);
 });
 
 // Create a new Person.
 
-controller.post('/', (req, res) => {
+controller.post('/', (req, res, next) => {
+  const { last_name, first_name, email } = req.body;
+  if (!last_name || !first_name || !email) {
+    return next(
+      new createError.BadRequest("Missing properties!")
+    );
+  }
   const newPerson = req.body;
   newPerson.id = data[data.length - 1].id + 1;
   data.push(newPerson);
@@ -29,11 +39,16 @@ controller.post('/', (req, res) => {
 
 // Update a person.
 
-controller.put('/:id', (req, res) => {
+controller.put('/:id', (req, res, next) => {
   const id = req.params.id;
   const index = data.findIndex(p => p.id === Number(id));
   const { first_name, last_name, email } = req.body;
-  
+  if (!last_name || !first_name || !email) {
+    return next(
+      new createError.BadRequest("Missing properties!")
+    );
+  }
+
   data[index] = {
     id,
     first_name,
@@ -41,14 +56,17 @@ controller.put('/:id', (req, res) => {
     email
   };
 
-  
+
   res.json(data[index]);
 });
 
 // Delete a person.
 
-controller.delete('/:id', (req, res) => {
+controller.delete('/:id', (req, res, next) => {
   const index = data.findIndex(p => p.id === Number(req.params.id));
+  if (index === -1) {
+    return next(new createError.NotFound("Person is not found"));
+  }
   data.splice(index, 1);
   res.json({});
 });
--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use('/person', require('./controllers/person/routes'));
 
+app.use((err, req, res, next) => {
+  res.status(err.statusCode);
+  res.json({
+    hasError: true,
+    message: err.message
+  });
+});
+
 app.listen(port, () => {
   console.log(`App is listening at http://localhost:${port}`);
 });
