3948cd4757a8e171e96d4f21af207354088bdf23
sistiri <sistiri@yahoo.com>
2021-07-15 16:54:28 +0200

    14 - JEST - Test Service

---
+++ src/controllers/person/person.service.js
@@ -0,0 +1,14 @@
+const Person = require('../../models/person.model');
+
+export.create = personData => {
+  const person = new Person(personData);
+  return person.save
+}
+
+exports.findAll = () => Person.find();
+
+exports.findOne = id => Person.findById(id);
+
+exports.update = (id, updateData) => Person.findByIdAndUpdate(id, updateData, { new: true });
+
+exports.delete = id => Person.findOneAndRemove(id);
