83dc604c8f2f5a9e073c5aa9be65001a3ddaf5e7
sistiri <sistiri@yahoo.com>
2021-07-15 14:56:36 +0200

    07-MongoDB Atlas Connection with mongoose

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const logger = require('./config/logger')
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
 const port = 3000;
+
+// Database connection
+mongoose
+  .connect('mongodb+srv://NodeAdminUser:MongoDBAtlasAdmin@cluster0.tgvgo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => logger.info('MongoDB connection has been established successfully'))
+  .catch(err => {
+    logger.error(err);
+    process.exit()
+  });
 
 app.use(morgan('combined', { stream: logger.stream }));
 
