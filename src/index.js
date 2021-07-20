7a2a3676d492c7da92d3ed4bfd6aada1356ee127
sistiri <sistiri@yahoo.com>
2021-07-20 16:52:13 +0200

    19 - Integration Test - Prep - supertest, server.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 require('dotenv').config();
 const config = require('config');
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const logger = require('./config/logger')
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+const logger = require('./config/logger');
+const app = require('./server');
 
 const port = process.env.PORT || 3000;
 
@@ -16,33 +11,6 @@ if (!config.has('database')) {
   logger.error('No database config found.');
   process.exit();
 }
-const { username, password, host } = config.get('database');
-mongoose
-  .connect(`mongodb+srv://${username}:${password}@${host}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => logger.info('MongoDB connection has been established successfully'))
-  .catch(err => {
-    logger.error(err);
-    process.exit()
-  });
-
-app.use(morgan('combined', { stream: logger.stream }));
-
-// app.use('/images', express.static('./images'));
-app.use(express.static('public'));
-
-app.use(bodyParser.json());
-app.use('/person', require('./controllers/person/person.routes'));
-
-app.use((err, req, res, next) => {
-  res.status(err.statusCode);
-  res.json({
-    hasError: true,
-    message: err.message
-  });
-});
 
 app.listen(port, () => {
   console.log(`App is listening at http://localhost:${port}`);
---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const config = require('config');
+const logger = require('./config/logger');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const { username, password, host } = config.get('database');
+mongoose
+  .connect(`mongodb+srv://${username}:${password}@${host}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => logger.info('MongoDB connection has been established successfully'))
+  .catch(err => {
+    logger.error(err);
+    process.exit()
+  });
+
+app.use(morgan('combined', { stream: logger.stream }));
+
+// app.use('/images', express.static('./images'));
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use('/person', require('./controllers/person/person.routes'));
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode);
+  res.json({
+    hasError: true,
+    message: err.message
+  });
+});
+
+
+module.exports = app;
