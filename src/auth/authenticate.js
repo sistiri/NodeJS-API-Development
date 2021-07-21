9cbbe0bbe0077c0ab4287e7f852b6bc90664361a
sistiri <sistiri@yahoo.com>
2021-07-22 00:09:46 +0200

    26 - JWT Authentication - Preparation

---
+++ src/auth/authenticate.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    // Bearer lkdajhhaisgduajwdbujqyuwehd
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    })
+  } else {
+    res.sendStatus(401);
+  }
+}
